a0ab010593e1234cc44ee5a63b162c32fc41b9c8
nglvnyls <nglvinyls@pm.me>
2020-12-09 13:02:17 +0100

    first commit

---
+++ src/components/bloc1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// styles
+const bloc = {
+    color: "#232129",
+    padding: "96px",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+    background-color: "#000022"
+    min-height: 100vp;
+    width: 100% 
+  }
+
+export default function Bloc1() {
+    return (
+        <div style={bloc}>
+            <h1>manual per a pares</h1>
+        </div>
+    )
+}
